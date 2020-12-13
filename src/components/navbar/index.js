f926a85be916b77fd170d7328ece82be53719f19
Sardar1208 <sarthak.bakre@gmail.com>
2020-12-13 18:16:58 +0530

    feat: modidropdown

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,16 +3,17 @@ import { Navbar, Form, Nav, Button, FormControl } from "react-bootstrap";
 export function ModiNavbar(props) {
   return (
     <Navbar bg="dark" variant="dark" fixed="top">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+      <Nav>
+        {props.dropdown}
       </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
+      <Nav class = "mx-auto">
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-12" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Nav>
+
     </Navbar>
   );
 }
